304b66a6a0a7822c4077edf43a9e6a52a6ac568a
Cody Myers <codymyers63@gmail.com>
2018-01-10 19:22:05 +0000

    Added the model architecture

---
+++ db/models.py
@@ -0,0 +1,27 @@
+from peewee import *
+import os
+
+mainDB = SqliteDatabase('data.sqlite',
+                        pragmas = ( ('busy_timeout', 100),
+                                    ('journal_mode', 'WAL')),
+                                    threadlocals = True)
+                                    
+class dbModel(Model):
+    class Meta:
+        database = mainDB
+        
+class Community(dbModel):
+    name = CharField(primary_key=True)
+    
+    def __str__(self):
+        return str(self.name)
+    
+class Comments(dbModel):
+    CID      = PrimaryKeyField()
+    community = ForeignKeyField(Community)
+    sentence  = CharField()
+    neg       = FloatField()
+    neu       = FloatField()
+    pos       = FloatField()
+    compound  = FloatField()
+    cm_type   = CharField(null = True)
